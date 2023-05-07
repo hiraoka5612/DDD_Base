c1ae23e4e8d81596394280f534b7a45e2ecc7e51
hiraoka5612 <hiraoka.sinichiro@gmail.com>
2023-05-08 00:37:06 +0900

    ログオフの処理を追加

--- DDD_Base.WinForm/Views/UserVIew.Designer.cs
+++ DDD_Base.WinForm/Views/UserVIew.Designer.cs
@@ -28,11 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.UserIdTextBox = new System.Windows.Forms.TextBox();
             this.UserNameTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.SearchButton = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.CloseLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,7 +43,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(31, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 27);
+            this.label1.Size = new System.Drawing.Size(84, 18);
             this.label1.TabIndex = 0;
             this.label1.Text = "ユーザーID";
             // 
@@ -65,7 +68,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(31, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(129, 27);
+            this.label2.Size = new System.Drawing.Size(86, 18);
             this.label2.TabIndex = 2;
             this.label2.Text = "ユーザー名";
             // 
@@ -79,11 +82,25 @@
             this.SearchButton.UseVisualStyleBackColor = true;
             this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // CloseLabel
+            // 
+            this.CloseLabel.AutoSize = true;
+            this.CloseLabel.Location = new System.Drawing.Point(41, 225);
+            this.CloseLabel.Name = "CloseLabel";
+            this.CloseLabel.Size = new System.Drawing.Size(52, 18);
+            this.CloseLabel.TabIndex = 5;
+            this.CloseLabel.Text = "label3";
+            // 
             // UserVIew
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(521, 259);
+            this.Controls.Add(this.CloseLabel);
             this.Controls.Add(this.SearchButton);
             this.Controls.Add(this.UserNameTextBox);
             this.Controls.Add(this.label2);
@@ -91,6 +108,8 @@
             this.Controls.Add(this.label1);
             this.Name = "UserVIew";
             this.Text = "UserVIew";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UserView_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.UserView_MouseClick);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -103,5 +122,7 @@
         private System.Windows.Forms.TextBox UserNameTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label CloseLabel;
     }
 }
--- DDD_Base.WinForm/Views/UserVIew.cs
+++ DDD_Base.WinForm/Views/UserVIew.cs
@@ -9,9 +9,12 @@ namespace DDD_Base.WinForm.Views
     public partial class UserVIew : Form
     {
         private UserViewModel _viewModel = new UserViewModel();
+        private int countdownSeconds = 10;
         public UserVIew()
         {
             InitializeComponent();
+            timer1.Interval = 1000;
+            timer1.Start();
 
             UserIdTextBox.DataBindings.Add("Text", _viewModel, nameof(_viewModel.IdText));
             UserNameTextBox.DataBindings.Add("Text", _viewModel, nameof(_viewModel.NameText));
@@ -20,6 +23,33 @@ namespace DDD_Base.WinForm.Views
         private void SearchButton_Click(object sender, EventArgs e)
         {
             _viewModel.Search();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            countdownSeconds--;
+            if(countdownSeconds < 0)
+            {
+                timer1.Stop();
+                this.Close();
+            } else
+            {
+                CloseLabel.Text ="残り" +  countdownSeconds.ToString() + "秒で画面をログオフします。";
+            }
+        }
+
+        private void UserView_KeyDown(object sender, KeyEventArgs e)
+        {
+            timer1.Stop();
+            countdownSeconds = 10;
+            timer1.Start();
+        }
+
+        private void UserView_MouseClick(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+            countdownSeconds = 10;
+            timer1.Start();
         }
     }
 }
